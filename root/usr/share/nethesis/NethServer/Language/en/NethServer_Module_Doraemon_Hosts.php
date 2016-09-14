52904028d650c91df6e46922174ee53ca97e5018
paspo <paolo@asperti.com>
2016-09-14 09:06:11 +0200

    translations updated

---
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Doraemon_Hosts.php
@@ -0,0 +1,7 @@
+<?php
+
+/* /usr/share/nethesis/NethServer/Language/en/NethServer_Module_Doraemon_Hosts.php */
+
+$L['Hosts_Title'] = 'Hosts';
+$L['Create_Host_Header'] = 'Create a new host';
+$L['Update_Host_Header'] = 'Update host "${0}"';
---
+++ root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_Doraemon_Hosts.php
@@ -0,0 +1,7 @@
+<?php
+
+/* /usr/share/nethesis/NethServer/Language/it/NethServer_Module_Doraemon_Hosts.php */
+
+$L['Hosts_Title'] = 'Hosts';
+$L['Create_Host_Header'] = 'Crea un nuovo host';
+$L['Update_Host_Header'] = 'Modifica il nome host "${0}"';
